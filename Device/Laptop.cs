c91153f0a2336a437f27c1f4c574f8f9aa619364
Jia <jiavon@ualberta.ca>
2020-09-04 13:48:32 -0600

    Completed Laptop: Device class, all errors resolved and according to assignment spec.

--- Device/Laptop.cs
+++ Device/Laptop.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Computer_Shop.Peripheral;
 
 namespace Computer_Shop
 {
-    /*
-     Create a “Laptop” class derived from “Device” with the following:
-        A “Screen” property that must have a “Screen” object with the “Integrated” connector type assigned.
-        A “Keyboard” property that must have a “Keyboard” object with the “Integrated” connector type assigned.
-        A default and greedy constructor.
-     */
-    public class Laptop : Device
+    public class Laptop : Device //Create a “Laptop” class derived from “Device” with the following:
     {
+        //A “Screen” property that must have a “Screen” object with the “Integrated” connector type assigned.
+        public Screen Screen { get; set; }
+
+        //A “Keyboard” property that must have a “Keyboard” object with the “Integrated” connector type assigned.
+        public Keyboard Keyboard { get; set; }
+
+        //Overrride accoding to the class example
+        public override string Brand { get; set; }
+
+        //A default and greedy constructor.
+        public Laptop(Screen screen, Keyboard keyboard, string brand)
+        {
+            Screen = screen;
+            Keyboard = keyboard;
+            Brand = brand;
+        }
 
     }
 }
