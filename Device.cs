7ba53a8c043510aa3b19c18d20fa503c4bba4e49
Jia <jiavon@ualberta.ca>
2020-09-04 15:18:47 -0600

    Tested the new Laptop, and the output was as expected.

--- Device.cs
+++ Device.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Computer_Shop.Peripheral;
 
@@ -18,17 +19,22 @@ namespace Computer_Shop
         public Memory Memory { get; set; }
 
         // A “Connectors” property (a read-only dictionary keyed with values of the Connector enumeration and valued with ints to represent the number of connections available)
-        public ReadOnlyMemory<Peripheral.Peripheral.Connector> Connectors { get; set; }
+        public ReadOnlyDictionary<Peripheral.Peripheral.Connector, int> Connectors { get; set; }
 
         //A “Peripherals” property (a polymorphic list of Peripheral objects) private
         private List<Peripheral.Peripheral> Peripherals { get; set; }
 
-
         /*
-         A “ConnectPeripheral()” method that accepts a polymorphic peripheral argument and:
+            A “ConnectPeripheral()” method that accepts a polymorphic peripheral argument and:
             Throws an exception if there are no available connectors of the connection-type of the peripheral.
             Unless the type is “Integrated”.
             Otherwise, adds the peripheral to the list.
+
          */
+
+        public void ConnectedPeripheral(Peripheral.Peripheral peripheral)
+        {
+            Console.WriteLine(peripheral);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Computer_Shop.Peripheral;
 
 namespace Computer_Shop
 {
@@ -6,7 +7,27 @@ namespace Computer_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Laptop(Screen screen, Keyboard keyboard, string brand)
+            //Screen(int width, int height, Connector connectorType, string brand)
+            //public Keyboard(Type keyType, Connector connectorType, string brand)
+
+            /*
+         public enum Connector
+        {
+            USBTypeA,
+            USBTypeB,
+            USBTypeC,
+            MiniUSB,
+            MicroUSB,
+            DisplayPort,
+            Integrated,
+        }*/
+            Laptop newComp = new Laptop(new Screen(1024, 600, Peripheral.Peripheral.Connector.DisplayPort, "Asus"), 
+                                        new Keyboard(Keyboard.Type.Mechanical, Peripheral.Peripheral.Connector.USBTypeA, "Walmart"),
+                                        "Toshiba");
+            Console.WriteLine($"This laptop have screen brand {newComp.Screen.Brand}, with width {newComp.Screen.Width} and height {newComp.Screen.Height}. The connector type is {newComp.Screen.ConnectorType} \n" +
+                $"The keyboard brand is {newComp.Keyboard.Brand} and the keyboard type is {newComp.Keyboard.KeyType}. The connector type is {newComp.Keyboard.ConnectorType}. \n" +
+                $"The brand of the entire laptop is {newComp.Brand}");
         }
     }
 }
