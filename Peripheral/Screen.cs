2969a3ee261947610e9b02d66037562075d8fd42
Jia <jiavon@ualberta.ca>
2020-09-04 13:40:22 -0600

    Completed peripherals class and the children of peripheral class

---
+++ Peripheral/Screen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computer_Shop.Peripheral
+{
+    public class Screen : Peripheral //Create a “Screen” class derived from “Peripheral” with the following:
+    {
+        public override string Brand { get; } //created for improved security and FUN! THEY SAY!
+
+        //A “Width” property, in whole pixels (int).
+        public int Width { get; set; }
+
+        //A “Height” property, in whole pixels (int).
+        public int Height { get; set; }
+
+        //A “ConnectorType” with a value of the “Connector” enumeration.
+        public Connector ConnectorType { get; set; }
+
+        //A default and greedy constructor.
+        public Screen(int width, int height, Connector connectorType, string brand)
+        {
+            Width = width;
+            Height = height;
+            ConnectorType = connectorType;
+            Brand = brand;
+        }
+    }
+}
